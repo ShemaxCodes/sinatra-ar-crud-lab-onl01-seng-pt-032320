6b9f76909beea1b6f7c7aaa75f1a79161784a6b9
Jashema Panapa <jcpanapa@gmail.com>
2020-06-25 18:17:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,7 +49,7 @@ class ApplicationController < Sinatra::Base
   end
   
   delete '/articles/:id' do 
-    @article = Article.find_by(id: params[:id])
+    @article = Article.find(params[:id])
     @article.delete
     redirect '/articles'
     
