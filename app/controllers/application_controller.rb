f7e11a47f599d79bcf50a3342398e1ff46237a0c
Jashema Panapa <jcpanapa@gmail.com>
2020-06-25 18:02:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   end 
   
   get '/articles/:id' do
-    @article = Article.find(params[:id])
+    @article = Article.find_by(params[:id])
     erb :show
   end
   
